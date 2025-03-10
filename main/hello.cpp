b011f5bb65b6d86c4a52657f491f7f18f89fd107
atharvakinage <kinageatharv@gmail.com>
2025-03-10 16:24:29 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
-  cout << "Hello, this is PES2UG22CS112, Atharva Amol Kinage" \n";
+  cout << "Hello, this is PES2UG22CS112, Atharva Amol Kinage \n";
   return 0;
 }
